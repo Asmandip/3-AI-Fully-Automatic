e25711becd46c7e7f6d58653f0e1abb3ec8d4839
Asmandip <asmandip@gmail.com>
2025-08-18 08:43:12 +0530

    patch_dash.py

--- patch_dash.py
+++ patch_dash.py
@@ -2,6 +2,5 @@ from dash_extensions.enrich import DashProxy as OriginalDashProxy
 
 class PatchedDashProxy(OriginalDashProxy):
     def __init__(self, *args, **kwargs):
-        # async_callbacks প্যারামিটার থাকলে তা সরিয়ে ফেলুন
         kwargs.pop("async_callbacks", None)
         super().__init__(*args, **kwargs)
