c7096336257928d15a3497c072db5f31c2d19cde
Asmandip <asmandip@gmail.com>
2025-08-18 10:09:21 +0530

    Delete patch_dash.py

--- patch_dash.py
+++
@@ -1,6 +0,0 @@
-from dash_extensions.enrich import DashProxy as OriginalDashProxy
-
-class PatchedDashProxy(OriginalDashProxy):
-    def __init__(self, *args, **kwargs):
-        kwargs.pop("async_callbacks", None)
-        super().__init__(*args, **kwargs)
